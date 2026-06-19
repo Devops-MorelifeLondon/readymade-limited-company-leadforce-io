8137b0b8b2eb0cd417827aee40a9534dd8400a03
Devops-MorelifeLondon <devops@morelifelondon.net>
2026-06-19 15:40:11 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,33 +68,7 @@ export default function RootLayout({
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "wm7t802jiw");
-            `,
-          }}
-        />
-
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-39ZLH2YMNT"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-39ZLH2YMNT');
-            `,
-          }}
-        />
-
-        {/* Google Ads */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-810525438"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-810525438');
+              })(window, document, "clarity", "script", "x9f99rd40e");
             `,
           }}
         />
